cbc3b14c28c883a61a4124d0139ecae99e6cc96f
wangkai <17875822605@163.com>
2023-01-17 17:18:10 +0800

    test update

--- test_.py
+++ test_.py
@@ -1,6 +1,5 @@
 import os
 
-
 # a = "KBE_RES_PATH = %KBE_ROOT%/kbe/res;%KBE_ROOT%/assets/;%KBE_ROOT%/assets/scripts;%KBE_ROOT%/assets/res/"
 # b = a.replace("/", "\\")
 
@@ -67,35 +66,123 @@ import os
 # A = ObcTest(a=123, b="test")
 # A.get_num()
 
+#
+# def binary_search(test_list, item, rev=False):
+#     """
+#
+#     :param test_list:
+#     :param item: 被搜索的数
+#     :param rev: 被搜索的数
+#     :return:
+#     """
+#     test_list = list(set(test_list))
+#     test_list.sort(reverse=False)
+#     low = 0
+#     high = len(test_list) - 1
+#     while low <= high:
+#         mid = (low + high) // 2  # 中间元素标记位
+#         guess = test_list[mid]
+#         print(f"guess --> {guess}", )
+#         if guess == item:
+#             return mid
+#         if guess > item:  # 如果中间数大于目标数 则中心数减1
+#             high = mid - 1
+#         else:
+#             low = mid + 1  # 如果中间数小于目标数 则中心数+1
+#     return None
 
-def binary_search(test_list, item, rev=False):
+
+# if __name__ == '__main__':
+#     list1 = [x for x in range(1, 99)]
+#     binary_search(list1, 65)
+# arr = [1, 123, 1999, 523, 42, 5, 4, 2, 324, 5, 2, 771, 77, 3, 4, 2, 4, 2, 7758]
+# arr2 = list(set(arr))
+# arr2.sort(reverse=True)
+# def fact(num):
+#     if num == 1:
+#         return 1
+#     else:
+#         return num * fact(num - 1)
+#
+#
+# if __name__ == '__main__':
+#     print(fact(9))
+
+# def fact(index, arry_1):
+#     """
+#     数组阶乘
+#     :param arry_1:
+#     :param index:
+#     :return:
+#     """
+#     if index == 0:
+#         return arry_1[0]
+#     else:
+#         return arry_1[index] * fact(index, arry_1[index - 1])
+
+# def quicksort(arr):
+#     if len(arr) < 2:
+#         return arr
+#     else:
+#         pivot = arr[0]
+#         less = [x for x in arr[1:] if x <= pivot]
+#         greater = [x for x in arr[1:] if x > pivot]
+#         print("pivot", pivot)
+#         print("less", less)
+#         print("greater", greater)
+#         return quicksort(less) + [pivot] + quicksort(greater)
+#
+#
+# if __name__ == '__main__':
+#     arr = [1, 123, 1999, 523, 42, 5, 4, 2, 324, 5, 2, 771, 77, 3, 4, 2, 4, 2, 7758]
+#     arr2 = list(set(arr))
+#     # arr2.sort(reverse=True)
+#     arr2.sort()
+#     print(quicksort(arr))
+#
+# graph = dict()
+# graph["you"] = ["alice", "bob", "claire"]
+# graph["start"] = dict()
+# graph["start"]["a"] = 6
+# graph["start"]["b"] = 2
+#
+# graph["a"] = {}
+# graph["a"]["fin"] = 1
+#
+# graph["b"] = {}
+# graph["b"]["a"] = 3
+# graph["b"]["fin"] = 5
+#
+# graph["fin"] = {}
+#
+# infinity = float("inf")
+# costs = {"a": 6, "b": 2, "fin": infinity}
+#
+# parents = {"a": "start", "b": "start", "fin": None}
+#
+#
+# def find_lowst_cost_node(costs):
+#     low_costs = float("inf")
+#     low_costs_node = None
+#     for node in costs:
+#         cost = costs[node]
+#         if cost < low_costs and node not in processed:
+#             low_costs = cost
+#             low_costs_node = node
+#     return low_costs_node
+
+fruits = set(["avocado", "tomato", "banana"])
+vagetables = set(["beets", "carrots", "tomato"])
+# print(fruits & vagetables)
+# print(fruits | vagetables)
+# print(fruits - vagetables)
+# print(vagetables - fruits)
+def test_func(states_needs,states_for_station):
     """
 
-    :param test_list:
-    :param item: 被搜索的数
-    :param rev: 被搜索的数
+    :param states_needs:
+    :param states_for_station:
     :return:
     """
-    test_list = list(set(test_list))
-    test_list.sort(reverse=False)
-    low = 0
-    high = len(test_list) - 1
-    while low <= high:
-        mid = (low + high) // 2  # 中间元素标记位
-        guess = test_list[mid]
-        print(f"guess --> {guess}", )
-        if guess == item:
-            return mid
-        if guess > item:  # 如果中间数大于目标数 则中心数减1
-            high = mid - 1
-        else:
-            low = mid + 1  # 如果中间数小于目标数 则中心数+1
-    return None
-
-
-if __name__ == '__main__':
-    list1 = [x for x in range(1, 99)]
-    binary_search(list1, 65)
-    # arr = [1, 123, 1999, 523, 42, 5, 4, 2, 324, 5, 2, 771, 77, 3, 4, 2, 4, 2, 7758]
-    # arr2 = list(set(arr))
-    # arr2.sort(reverse=True)
+    cover = states_needs & states_for_station
+    if len(cover) > len
